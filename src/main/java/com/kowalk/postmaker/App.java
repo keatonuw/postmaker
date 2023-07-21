6ee2ab68079e93546c827815ac5f32df88ac01fb
keatonuw <keatonkowal@icloud.com>
2023-07-20 23:04:37 -0700

    update main

--- src/main/java/com/kowalk/postmaker/App.java
+++ src/main/java/com/kowalk/postmaker/App.java
@@ -2,6 +2,7 @@ package com.kowalk.postmaker;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -9,25 +10,36 @@ import java.sql.SQLException;
  */
 public class App {
     public static void main(String[] args) throws SQLException, IOException {
-        if (args.length < 2) {
+        if (args.length < 4) {
             usage();
         }
 
-        String filename = args[1];
+        Scanner scanner = new Scanner(System.in);
 
-        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String filename = args[0];
+        String url = args[1];
+        String username = args[2];
+        String password = args[3];
 
         System.getenv("test");
 
-        DbService db = new DbService("postgres", "", url);
-        db.getAll();
+        DbService db = new DbService(username, password, url);
 
         String post = PostFileReader.read("./testfile.md");
 
         PostModel model = PostModel.parse(post);
-        model.spellCheck();
+        if (model.spellCheck()) {
+            System.out.println("Errors Found. Continue? [Y/n] ");
+            if (!scanner.next().equalsIgnoreCase("y")) {
+                System.exit(0);
+            }
+        }
         model.convertContentLinks();
         System.out.println(model);
+        System.out.println("Upload? [Y/n]");
+        if (scanner.next().equalsIgnoreCase("y")) {
+            db.insertPost(model);
+        }
     }
 
     private static void usage() {
---
+++ testfile.md
@@ -0,0 +1,4 @@
+# The title will be this
+## And the subtitle that
+> With a summary here.
+Followed by a body here!
